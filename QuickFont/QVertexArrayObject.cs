a585ea29afa459088e3250ae5cce44b89064f1dd
Stulov <stulovd@gmail.com>
2018-12-06 18:00:12 +0200

    cleanup QVertexArrayObject from unuseful data copy and hige garbage generation, now reallocationg array only if data ammount doubled

--- QuickFont/QVertexArrayObject.cs
+++ QuickFont/QVertexArrayObject.cs
@@ -8,18 +8,13 @@ namespace QuickFont
 {
     public class QVertexArrayObject : IDisposable
     {
-        private const int InitialSize = 1000;
-        private int _bufferSize;
-        private int _bufferMaxVertexCount;
-        public int VertexCount;
         
-        private int VBOID;
-
-        public readonly SharedState QFontSharedState;
-
-        private List<QVertex> Vertices;
-        private QVertex[] VertexArray;
-
+        private int _vertexCount;  //current collected vertex count in buffer
+        private int _vertexCapacity;
+        private int _vboDataCapacity;
+        private int _vboid;
+        private readonly SharedState _qFontSharedState;
+        private QVertex[] _vertexArray;
         private static readonly int QVertexStride;
 
         static QVertexArrayObject()
@@ -29,98 +24,98 @@ namespace QuickFont
 
         public QVertexArrayObject(SharedState state)
         {
-            QFontSharedState = state;
+            _qFontSharedState = state;
+            _vertexCapacity = 1000;
+            _vertexArray = new QVertex[_vertexCapacity];
+            _vboDataCapacity = 0;
 
-            Vertices = new List<QVertex>(InitialSize);
-            _bufferMaxVertexCount = InitialSize;
-            _bufferSize = _bufferMaxVertexCount * QVertexStride;
-            
-            GL.UseProgram(QFontSharedState.ShaderVariables.ShaderProgram);
-
-            GL.GenBuffers(1, out VBOID);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VBOID);
-            EnableAttributes();
-
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)_bufferSize, IntPtr.Zero, BufferUsageHint.StreamDraw );
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.UseProgram(_qFontSharedState.ShaderVariables.ShaderProgram);
+            GL.GenBuffers(1, out _vboid);
         }
 
+        private void AllocSpace(int count)
+        {
+            if (_vertexCapacity < count)
+            {
+                while(_vertexCapacity < count)
+                    _vertexCapacity *= 2;
+
+                Array.Resize(ref _vertexArray,_vertexCapacity);
+            }
+        }
         internal void AddVertexes(IList<QVertex> vertices)
         {
-            VertexCount += vertices.Count;
-            Vertices.AddRange(vertices);
+            if (vertices.Count != 0)
+            {
+                AllocSpace(vertices.Count + _vertexCount);
+                vertices.CopyTo(_vertexArray, _vertexCount);
+                _vertexCount += vertices.Count;
+            }
         }
 
-        public void AddVertex(Vector3 position, Vector2 textureCoord, Vector4 colour)
+        internal void AddVertex(Vector3 position, Vector2 textureCoord, Vector4 colour)
         {
-            VertexCount++;
-            Vertices.Add(new QVertex
+            AllocSpace(_vertexCount+1);
+            _vertexArray[_vertexCount++] = new QVertex
             {
                 Position = position,
                 TextureCoord = textureCoord,
                 VertexColor = colour
-            });
+            };
         }
 
         public void Load()
         {
-            if (VertexCount == 0)
+            if (_vertexCount == 0)
                 return;
-            VertexArray = Vertices.ToArray();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VBOID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboid);
             EnableAttributes();
 
-            if (VertexCount > _bufferMaxVertexCount)
+            var spaceneeded = _vertexCount * QVertexStride;
+            if (spaceneeded > _vboDataCapacity) //if no space then update with reallocation
             {
-                while (VertexCount > _bufferMaxVertexCount)
-                {
-                    _bufferMaxVertexCount += InitialSize;
-                    _bufferSize = _bufferMaxVertexCount*QVertexStride;
-                }
-
-                //reinitialise buffer with new _bufferSize
-                GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr) _bufferSize, IntPtr.Zero, BufferUsageHint.StreamDraw);
+                _vboDataCapacity = spaceneeded;
+                GL.BufferData(BufferTarget.ArrayBuffer, spaceneeded, _vertexArray, BufferUsageHint.StreamDraw);
             }
-            GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, (IntPtr) (VertexCount*QVertexStride), VertexArray);
+            else
+                GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, spaceneeded, _vertexArray);
         }
 
         public void Reset()
         {
-            Vertices.Clear();
-            VertexCount = 0;
+            _vertexCount = 0;
         }
 
         public void Bind()
         {
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VBOID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboid);
             EnableAttributes();
         }
 
         public void Dispose()
         {
-            GL.DeleteBuffers(1, ref VBOID);
+            GL.DeleteBuffers(1, ref _vboid);
         }
 
         public void DisableAttributes()
         {
-            GL.DisableVertexAttribArray(QFontSharedState.ShaderVariables.PositionCoordAttribLocation);
-            GL.DisableVertexAttribArray(QFontSharedState.ShaderVariables.TextureCoordAttribLocation);
-            GL.DisableVertexAttribArray(QFontSharedState.ShaderVariables.ColorCoordAttribLocation);
+            GL.DisableVertexAttribArray(_qFontSharedState.ShaderVariables.PositionCoordAttribLocation);
+            GL.DisableVertexAttribArray(_qFontSharedState.ShaderVariables.TextureCoordAttribLocation);
+            GL.DisableVertexAttribArray(_qFontSharedState.ShaderVariables.ColorCoordAttribLocation);
         }
 
         private void EnableAttributes()
         {
             int stride = QVertexStride;
-            GL.EnableVertexAttribArray(QFontSharedState.ShaderVariables.PositionCoordAttribLocation);
-            GL.EnableVertexAttribArray(QFontSharedState.ShaderVariables.TextureCoordAttribLocation);
-            GL.EnableVertexAttribArray(QFontSharedState.ShaderVariables.ColorCoordAttribLocation);
-            GL.VertexAttribPointer(QFontSharedState.ShaderVariables.PositionCoordAttribLocation, 3,
+            GL.EnableVertexAttribArray(_qFontSharedState.ShaderVariables.PositionCoordAttribLocation);
+            GL.EnableVertexAttribArray(_qFontSharedState.ShaderVariables.TextureCoordAttribLocation);
+            GL.EnableVertexAttribArray(_qFontSharedState.ShaderVariables.ColorCoordAttribLocation);
+            GL.VertexAttribPointer(_qFontSharedState.ShaderVariables.PositionCoordAttribLocation, 3,
                 VertexAttribPointerType.Float, false, stride, IntPtr.Zero);
-            GL.VertexAttribPointer(QFontSharedState.ShaderVariables.TextureCoordAttribLocation, 2, VertexAttribPointerType.Float,
+            GL.VertexAttribPointer(_qFontSharedState.ShaderVariables.TextureCoordAttribLocation, 2, VertexAttribPointerType.Float,
                 false,
                 stride, new IntPtr(3*sizeof (float)));
-            GL.VertexAttribPointer(QFontSharedState.ShaderVariables.ColorCoordAttribLocation, 4, VertexAttribPointerType.Float,
+            GL.VertexAttribPointer(_qFontSharedState.ShaderVariables.ColorCoordAttribLocation, 4, VertexAttribPointerType.Float,
                 false, stride, new IntPtr(5*sizeof (float)));
         }
     }
